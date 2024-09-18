9dcbddbd8e266367501f2035b040cdfdaee1a335
RajashekharReddyBheemreddy <bheemreddyrajashekhar@gmail.com>
2024-09-17 20:17:20 -0400

    added user profile features

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from './routes/MyUserRoutes';
+import {v2 as cloudinary} from 'cloudinary';
+import myRestaurantRoute from './routes/MyRestaurantRoute';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log('Connected to database'));
 const app = express();
@@ -10,10 +12,15 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/health', async (req: Request, res: Response) => {
-    res.send({message: 'health OK!'});
+    res.send({message: 'Health OK!'});
 });
-
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 app.use('/api/my/user', myUserRoute);
+app.use('/api/my/restaurant', myRestaurantRoute);
 
 app.listen(3000, () => {
     console.log("server Started")
